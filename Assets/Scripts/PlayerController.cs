9cc9fa1b0bb28ae20b65add9a3298a0e20f666e2
Hung Son <tranhungson.hn2004@gmail.com>
2025-05-27 02:11:44 +0700

    TsonDev_2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    
 
 
     Rigidbody2D rigid2d;
-    float horizontalMovement;
+   
     Animator animator;
-    Vector2 moveDirection =new Vector2(0,0);
-    
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    Vector2 moveDirection = new Vector2(0, 0);
+    float horizontalMovement;
+    [Header("Jump")]
+    public float jumpPower = 10f;
+    public int numberJump = 2;
+    int JumpRemaining;
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize =new Vector2 (0.5f,0.5f);
+    public LayerMask groundLayer;
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float MaxFallSpeed = 18f;
+    public float fallSpeed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +44,7 @@ public class PlayerController : MonoBehaviour
             moveDirection.Set(rigid2d.velocity.x,0);
             moveDirection.Normalize();
         }
-        if (moveDirection.x > 0)
-            transform.localScale = new Vector3(1, 1, 1); 
-        else if (moveDirection.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);  
         animator.SetFloat("MoveX", moveDirection.x);
-        Debug.Log(moveDirection.x);
         if (rigid2d.velocity.x == 0)
         {
             animator.SetInteger("Speed", 0);
@@ -44,10 +55,60 @@ public class PlayerController : MonoBehaviour
             animator.SetInteger("Speed", 1);
             
         }
+        GroundCheck();
+        Gravity();
+        animator.SetBool("isJump", !IsCheck());
 
     }
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+       
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (JumpRemaining > 0)
+        {
+            if (context.performed)
+            {
+                rigid2d.velocity = new Vector2(rigid2d.velocity.x, jumpPower);
+                JumpRemaining--;
+            }
+            else if (context.canceled)
+            {
+                rigid2d.velocity = new Vector2(rigid2d.velocity.x, rigid2d.velocity.y * 0.5f);
+                JumpRemaining--;
+            }
+        }
+
+    }
+    private bool IsCheck()
+    {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0.0f, groundLayer))
+        {
+            return true;
+        }  
+        return false;
+    }
+    private void GroundCheck()
+    {
+        if(IsCheck())
+        {
+            JumpRemaining = numberJump;
+        }        
+        
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+    void Gravity()
+    {
+        if (rigid2d.velocity.y < 0.0f)
+        {
+            rigid2d.gravityScale = baseGravity * fallSpeed;
+            rigid2d.velocity = new Vector2(rigid2d.velocity.x, Mathf.Max(rigid2d.velocity.y, -MaxFallSpeed));
+        }
     }
 }
