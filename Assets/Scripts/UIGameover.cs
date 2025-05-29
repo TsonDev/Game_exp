8403c2b0c8b20ca950cc53b55f67f2b452a18bd4
Hung Son <tranhungson.hn2004@gmail.com>
2025-05-29 11:14:48 +0700

    TsonDev_6

---
+++ Assets/Scripts/UIGameover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class UIGameover : MonoBehaviour
+{
+    private VisualElement gameOverPanel;
+    private Button retryButton;
+    
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        gameOverPanel = root.Q<VisualElement>("GameOverPanel");
+        retryButton = root.Q<Button>("PlayAgain"); // Đặt name="retry-button" trong UI Builder
+     
+
+        retryButton.clicked += () =>
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        };
+
+        HideGameOver();
+    }
+    public void ShowGameOver()
+    {
+        
+        gameOverPanel.style.display = DisplayStyle.Flex;
+        
+
+    }
+
+    public void HideGameOver()
+    {
+        
+        gameOverPanel.style.display = DisplayStyle.None;
+    }
+}
